16f163bf98169dfc80181a4db93026e693df5378
Colby Williams <ColbyLWilliams@gmail.com>
2024-05-21 15:32:47 -0500

    initial iOS project

---
+++ iOS/llexia/llexia/llexiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  llexiaApp.swift
+//  llexia
+//
+//  Created by Colby Williams on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct llexiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
